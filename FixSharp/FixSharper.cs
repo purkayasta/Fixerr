77ce3ae2fdcf606dce98906b3f6b5b90029af0d5
purkayasta <pritompurkayasta@gmail.com>
2022-03-08 23:43:28 +0600

    added symbol endpoint

--- FixSharp/FixSharper.cs
+++ FixSharp/FixSharper.cs
@@ -16,7 +16,7 @@ namespace FixSharp
         }
 
         /// <summary>
-        /// Get Latest Rates
+        /// Depending on your subscription plan, the API's latest endpoint will return real-time exchange rate data updated every 60 minutes, every 10 minutes or every 60 seconds.
         /// </summary>
         /// <param name="baseCurrency">Give the base currency, By Default base currency will be set to EUR by the API</param>
         /// <param name="symbols">Comma separated country's name</param>
@@ -32,6 +32,21 @@ namespace FixSharp
             var streamResponse = await _httpClient.GetStreamAsync(urlBuilder.ToString());
             var latestResponse = await JsonSerializer.DeserializeAsync<LatestRateResponse>(streamResponse);
             return latestResponse;
+        }
+
+        /// <summary>
+        /// The Fixer API comes with a constantly updated endpoint returning all available currencies. To access this list, make a request to the API's symbols endpoint.
+        /// </summary>
+        /// <returns></returns>
+        public async ValueTask<SymbolResponse> GetSymbolAsync()
+        {
+            StringBuilder urlBuilder = new();
+            urlBuilder.Append("latest");
+            urlBuilder.Append($"?access_key={FixerConstants.ApiKey}");
+
+            var streamResponse = await _httpClient.GetStreamAsync(urlBuilder.ToString());
+            var symbolResponse = await JsonSerializer.DeserializeAsync<SymbolResponse>(streamResponse);
+            return symbolResponse;
         }
     }
 }
---
+++ FixSharp/Models/SymbolResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FixSharp.Models
+{
+    public class SymbolResponse : ErrorResponse
+    {
+        [JsonPropertyName("success")]
+        public bool Success { get; set; }
+
+        [JsonPropertyName("symbols")]
+        public Dictionary<string, string> Symbols { get; set; }
+    }
+}
